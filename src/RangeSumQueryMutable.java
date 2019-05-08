c873762eb6be80b49f7987724ca81399e430ca95
3ylh3 <781706889@qq.com>
2019-05-08 14:50:45 +0800

    307

---
+++ src/RangeSumQueryMutable.java
@@ -0,0 +1,21 @@
+public class RangeSumQueryMutable {
+    class NumArray {
+        private int[] nums;
+
+        public NumArray(int[] nums) {
+            this.nums = nums;
+        }
+
+        public void update(int i, int val) {
+            nums[i] = val;
+        }
+
+        public int sumRange(int i, int j) {
+            int res = 0;
+            for(int index = i;index <= j;index++){
+                res += nums[index];
+            }
+            return res;
+        }
+    }
+}
